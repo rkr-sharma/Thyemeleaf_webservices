96494b990de78d48a7839a7fd87bcfc65ef9f0b5
Ram Krishna <113668437+rkr-sharma@users.noreply.github.com>
2022-12-02 16:52:30 +0530

    Add files via upload

---
+++ SpringBoot2ThymeleafCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.gopal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2ThymeleafCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2ThymeleafCrudAppApplication.class, args);
+	}
+
+}
